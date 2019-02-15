02331cac09e744ae885ee67de43a1720a54b4545
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2019-02-15 17:00:52 +0100

    feat: add template source code

---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module}}/__init__.py
@@ -0,0 +1,26 @@
+# Copyright (c) {{cookiecutter.year}}, {{cookiecutter.full_name}}.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Create top level imports."""
+
+
+__author__ = "{{cookiecutter.full_name}}"
+__email__ = "{{cookiecutter.email}}"
+from {{cookiecutter.project_module}}._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+
+from {{cookiecutter.project_module}}.helpers import show_versions
