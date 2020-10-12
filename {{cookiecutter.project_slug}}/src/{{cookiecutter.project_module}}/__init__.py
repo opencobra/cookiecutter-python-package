e6d5d8eb19e0aa7d4a589d8bd574c09dbc2097cd
Midnighter <midnighter@posteo.net>
2020-10-13 00:26:51 +0200

    refactor: switch from local script to versioneer-518

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module}}/__init__.py
@@ -18,9 +18,6 @@
 
 __author__ = "{{cookiecutter.full_name}}"
 __email__ = "{{cookiecutter.email}}"
-from {{cookiecutter.project_module}}._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
 
 
-from {{cookiecutter.project_module}}.helpers import show_versions
+from .helpers import show_versions
